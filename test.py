91b5442661123aee5391299427b775c559f0ba90
Avner Shanan <ashanan@gmail.com>
2014-10-06 01:35:25 -0500

    Read in a task to add

--- test.py
+++ test.py
@@ -11,29 +11,36 @@ def get_card(cards, title):
         if card.name == title:
             return card
 
-if __name__ == "__main__":
+def load_auth():
     auth = {}
     execfile("auth.config", auth) 
+    return auth
+
+def load_schedule():
+    schedule = {}
+    execfile("schedule.config", schedule) 
+    return schedule
+
+if __name__ == "__main__":
+    auth = load_auth()
+    scheduled_tasks = load_schedule()["tasks"]
 
     client = TrelloClient(api_key=auth["key"], api_secret=auth["secret"], token=auth["oauth_token"], token_secret=auth["oauth_token_secret"])
 
     boards = client.list_boards()
-    print boards
 
-    name = "Home"
-    cardTitle = "Laundry"
+    board_name = scheduled_tasks[0]["board"]
+    card_title = scheduled_tasks[0]["title"]
 
     for b in boards:
-        if b.name == name:
-            print b.name
-            print b.description
+        if b.name == board_name:
             todo_list = get_todo_list(b)
             cards = todo_list.list_cards()
-            print cards
 
-            card = get_card(cards, cardTitle)
+            card = get_card(cards, card_title)
             if card is None:
-                print "Card does not exist"
+                print "Card does not exist, adding it now."
+                todo_list.add_card(card_title, "Added by code!")
             else:
-                print card
+                print "That card already exists"
 
